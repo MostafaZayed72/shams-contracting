e3e22e805d97f8b8b0219eb16ef2f960a8f8eb34
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-11-27 14:26:16 +0200

    t

--- components/HomeCarousel.vue
+++ components/HomeCarousel.vue
@@ -3,7 +3,7 @@
     <v-carousel :height="carouselHeight" show-arrows="false" cycle hide-delimiter-background>
       <v-carousel-item>
         <div class="relative">
-          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto"
+          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer h-48 md:h-[400px] mx-auto"
             src="https://www.economy-today.com/economy/uploads/2021/09/%D8%A8%D8%B1%D8%AC-%D8%A7%D9%84%D9%85%D9%85%D9%84%D9%83%D8%A9-750x430.jpg" alt="Image" @click="navigateTo('/article')" />
 
         </div>
@@ -11,22 +11,22 @@
      
       <v-carousel-item>
         <div class="relative">
-          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto"
+          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer h-48 md:h-[400px] mx-auto"
             src="https://argaamplus.s3.amazonaws.com/961f0e5d-670c-4b01-8f2a-a5c0134fca43.png" alt="Image" @click="navigateTo('/article')" />
 
         </div>
       </v-carousel-item>
       <v-carousel-item>
         <div class="relative">
-          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto"
+          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer h-48  md:h-[400px] mx-auto"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Riyadh_Skyline.jpg/1200px-Riyadh_Skyline.jpg" alt="Image" @click="navigateTo('/article')" />
 
         </div>
       </v-carousel-item>
       <v-carousel-item>
         <div class="relative">
-          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto"
-            src="https://sona3masr.com/wp-content/uploads/2022/10/8aa37f29-7d84-41e8-b7ab-9160eeae85b5.jpg" alt="Image" @click="navigateTo('/article')" />
+          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer h-48  md:h-[400px] mx-auto"
+            src="https://m.eyeofriyadh.com/news_images/2018/03/1b41e0e013581.jpg" alt="Image" @click="navigateTo('/article')" />
 
         </div>
       </v-carousel-item>
