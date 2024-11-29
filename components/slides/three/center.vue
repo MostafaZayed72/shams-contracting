e41e3249cdabf8d568e831a5aed1edf273df18b2
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-11-29 20:58:58 +0200

    t

--- components/slides/three/center.vue
+++ components/slides/three/center.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col md:flex-row gap-4">
       <!-- div one -->
-      <div class="flex flex-col mx-4 md:mx-2 gap-2 mt-[700px] md:mt-0 items-center justify-center">
+      <div class="flex flex-col mx-4 md:mx-2 gap-2 mt-[1000px] md:mt-0 items-center justify-center">
         <img src="https://www.el-seif.com.sa/uploads/2023/08/king-1.webp" class="rounded-xl w-64 h-64 object-cover" alt="">
         <h1 class="flex items-center gap-2 mx-4 text-black">
           <Icon name="solar:buildings-3-linear" /> Kingdom Tower
