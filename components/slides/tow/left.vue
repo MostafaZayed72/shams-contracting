39fa143bcacd1ed15f5f7901b98925e16c4b1198
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-12-01 00:40:16 +0200

    t

--- components/slides/tow/left.vue
+++ components/slides/tow/left.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col  gap-10 mx-4">
+    <div class="flex flex-col  gap-10 mx-4 mb-10">
         <CounterStats />
         <img src="/public/imgs/second-slide.webp" alt="" class="rounded-xl mx-2 md:mx-0">
     </div>
